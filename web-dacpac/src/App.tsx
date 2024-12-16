52dcbaeb5500a8407029c2fed7ef28da55d0743f
Pablo QB <qb.pablo.2000@gmail.com>
2024-12-16 01:12:18 +0100

    Use router

--- web-dacpac/src/App.tsx
+++ web-dacpac/src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './componets/footer/Footer'
 import Header from './componets/header/Header'
@@ -5,17 +6,26 @@ import Main from './componets/main/Main'
 
 function App() {
   return (
-    <div className='d-flex flex-column justify-content-between vh-100'>
-      <div className="parent-container">
-        <div className="main-container">
-          <div className="child-container">
-            <Header />
-            <Main />
-            <Footer />
+    <BrowserRouter basename={window.location.pathname || ''}>
+      <Routes>
+        
+        <Route path="/" element={
+          
+          <div className='d-flex flex-column justify-content-between vh-100'>
+            <div className="parent-container">
+              <div className="main-container">
+                <div className="child-container">
+                  <Header />
+                  <Main />
+                  <Footer />
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        
+        } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
